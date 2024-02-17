eeb0393a22265858cc3d00fb6c5eba741bf5b6e1
kdima90 <kdima19901990@gmail.com>
2024-02-17 21:19:11 +0100

    set default route to home

--- src/main/java/kdima90/views/home/Home.java
+++ src/main/java/kdima90/views/home/Home.java
@@ -3,10 +3,12 @@ package kdima90.views.home;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import kdima90.views.MainLayout;
 
 @PageTitle("Home")
 @Route(value = "home", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
 public class Home extends VerticalLayout {
 
     public Home() {
